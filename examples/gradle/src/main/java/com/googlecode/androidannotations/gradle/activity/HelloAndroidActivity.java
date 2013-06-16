66ae82cf4268035309daaa4bcd212abc7eaa60ea
sinojelly <sinojelly@gmail.com>
2013-06-16 15:00:12 +0800

    compile with android 17. fix compile error in gradle example.

--- examples/gradle/src/main/java/com/googlecode/androidannotations/gradle/activity/HelloAndroidActivity.java
+++ examples/gradle/src/main/java/com/googlecode/androidannotations/gradle/activity/HelloAndroidActivity.java
@@ -2,10 +2,10 @@ package com.googlecode.androidannotations.gradle.activity;
 
 import android.app.Activity;
 import android.widget.TextView;
-import com.googlecode.androidannotations.annotations.AfterViews;
-import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.ViewById;
-import com.googlecode.androidannotations.annotations.res.StringRes;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.res.StringRes;
 import com.googlecode.androidannotations.gradle.R;
 
 import java.util.Date;
